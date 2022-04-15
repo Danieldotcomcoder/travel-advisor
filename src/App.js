c2e9225b1a1c796f9bb36ab147840b77080e3c75
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-04-15 16:09:45 +0300

    Create main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { CssBaseLine, Grid } from '@material-ui/core' 
+
+import Header from './components/Header/Header';
+import List from './components/List/Lists';
+import Map from './components/Map/Map'; 
+
 
 const App = () => {
     return (
 
         <div>
-            <h1>Hello, Woeld</h1>
+            <h1>Hello, World</h1>
         </div>
     );
 }
